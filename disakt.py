eeb46c6b28a0a524fa864f7a7408211ae2513c10
jivan <jramjisingh@albany.edu>
2020-06-15 22:27:51 -0400

    Initial RPC connection

--- disakt.py
+++ disakt.py
@@ -1,21 +1,36 @@
 from urllib.request import urlopen, Request
+from pypresence import Presence
+import time
 import json
 import credentials
-
-clientID=credentials.clientid
 
 headers = {
   'Content-Type': 'application/json',
   'trakt-api-version': '2',
-  'trakt-api-key': clientID
+  'trakt-api-key': credentials.traktclientid
 }
-request = Request('https://api.trakt.tv/users/jivandabeast/watching', headers=headers)
-response_body = urlopen(request).read()
 
+RPC = Presence(credentials.discordclientid)
+RPC.connect()
+# RPC.close()
 
-data = json.loads(response_body)
-print(data)
-print()
-print('The content type is:', data['type'])
-print('The episode title is:', data['episode']['title'])
-print('The show is:', data['show']['title'])
+def updateRPC(state, details, starttime, endtime):
+    RPC.update(state=state, details=details, start=starttime, end=endtime, large_image="trakt", small_image="trakt")
+
+while True:
+    request = Request('https://api.trakt.tv/users/jivandabeast/watching', headers=headers)
+    response_body = urlopen(request).read()
+    data = json.loads(response_body)
+
+    if(data['type'] == 'episode'):
+        print('The episode title is:', data['episode']['title'])
+        print('The show is:', data['show']['title'])
+        print('The TVDB ID is:', data['show']['ids']['tvdb'])
+        newstate="Watching a Show."
+    elif(data['type'] == 'movie'):
+        print('The name of the movie is:', data['movie']['title'])
+        print('The TMDB ID is:', data['movie']['ids']['tmdb'])
+        newstate="Watching a Movie"
+    else:
+        print('Ya fucked bud')
+    time.sleep(15)
