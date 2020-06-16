845f5026a052b4a472ffe5c10f4c19578a62de1d
jivan <jramjisingh@albany.edu>
2020-06-15 21:08:26 -0400

    Trakt API and JSON manipulation

--- disakt.py
+++ disakt.py
@@ -1,4 +1,21 @@
+from urllib.request import urlopen, Request
+import json
 import credentials
-from trakt import Trakt
+
+clientID=credentials.clientid
+
+headers = {
+  'Content-Type': 'application/json',
+  'trakt-api-version': '2',
+  'trakt-api-key': clientID
+}
+request = Request('https://api.trakt.tv/users/jivandabeast/watching', headers=headers)
+response_body = urlopen(request).read()
 
 
+data = json.loads(response_body)
+print(data)
+print()
+print('The content type is:', data['type'])
+print('The episode title is:', data['episode']['title'])
+print('The show is:', data['show']['title'])
